b9f08eca1f38766a734e3307f1f396c98d736f54
gymmer <511470840@qq.com>
2017-05-24 17:54:32 +0800

    Use sourcemap and url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ var ENTRY_HTML_FILE = path.resolve(SRC_PATH, 'index.html');
 // 开发环境/生成环境的不同配置项
 var OUT_PATH = isDev ? DEV_PATH : PRO_PATH;					// 构建路径
 var FILE_NAME_FORMAT = isDev ? '[name]' : '[name]-[hash]';	// 文件名是否hash
+var SOURCEMAP_TYPE = isDev ? 'source-map' : false;			// sourcemap类型
 var SCSS_STYLE = isDev ? 'expanded' : 'compress';			// SCSS编译风格
 var MINIFY_HTML_OPTIONS = isDev ? false : {					// 压缩HTML选项
 	removeComments: true,
@@ -59,8 +60,7 @@ module.exports = {
 	},
 
 	// 使用Source Map
-	// devtool: 'source-map',//配置生成Source Maps，选择合适的选项
-	// devtool: 'eval-source-map',//配置生成Source Maps，选择合适的选项
+	devtool: SOURCEMAP_TYPE,
 	
 	module: {
 
@@ -88,9 +88,10 @@ module.exports = {
 	      	// 添加对样式表的处理
 			{
 	        	test: /\.css$/,
+	        	// loader: 'style-loader!css-loader',
 		        loader: ExtractTextWebpackPlugin.extract({
 		        	fallback: 'style-loader',
-		        	use: 'css-loader'
+		        	use: `css-loader?sourceMap=true`
 	        	}),
 	        	include: SRC_PATH,
 	        	exclude: MOD_PATH
@@ -101,7 +102,7 @@ module.exports = {
 	        	test: /\.less$/,
 		        loader: ExtractTextWebpackPlugin.extract({
 		        	fallback: 'style-loader',
-		        	use: 'css-loader!less-loader'
+		        	use: `css-loader?sourceMap=true!less-loader?sourceMap=true`
 	        	}),
 	        	include: SRC_PATH,
 	        	exclude: MOD_PATH
@@ -112,10 +113,16 @@ module.exports = {
 	        	test: /\.(scss|sass)$/,
 		        loader: ExtractTextWebpackPlugin.extract({
 		        	fallback: 'style-loader',
-		        	use: `css-loader!ruby-sass-loader?compass=1&outputStyle=${SCSS_STYLE}`
+		        	use: `css-loader?sourceMap=true!ruby-sass-loader?compass=1&outputStyle=${SCSS_STYLE}&sourceMap=true`
 	        	}),
 	        	include: SRC_PATH,
 	        	exclude: MOD_PATH
+	      	},
+
+	      	// 图片规则
+	      	{
+	      		test: /\.(png|jpg|gif|jpeg|svg)$/i,
+	      		loader: `url-loader?limit=1000&name=img/bg/${FILE_NAME_FORMAT}.[ext]`
 	      	}
 
 		]
@@ -127,7 +134,6 @@ module.exports = {
 	devServer: {
 	    contentBase: OUT_PATH,	// 根目录
 	    inline: true, 			// 实时刷新
-	    hot: true,        		// 自动刷新
 	    port: 8080
 	},
 
@@ -143,7 +149,8 @@ module.exports = {
 		}),
 
 		// 提取样式文件
-		new ExtractTextWebpackPlugin(`css/${FILE_NAME_FORMAT}.css`),
+		// new ExtractTextWebpackPlugin(`css/${FILE_NAME_FORMAT}.css`),
+		new ExtractTextWebpackPlugin(`${FILE_NAME_FORMAT}.css`),
 
 		// 复制文件
 		new CopyWebpackPlugin([
